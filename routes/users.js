31e3799327a5aef52c987bf6854f9f9250a6c230
vanshverma721 <vvv.721781@gmail.com>
2024-01-19 19:57:30 +0530

    code changed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
 
 mongoose.connect("mongodb://localhost:27017/Pinterest");
 
 const userSchema = mongoose.Schema({
-
+    username: String,
+    name: String,
+    email: String,
+    password: String,
+    profileImage: String,
+    contact: Number,
+    boards: {
+        type: Array,
+        default: []
+    }
 });
+
+userSchema.plugin(plm);
 
 module.exports = mongoose.model("user", userSchema);
